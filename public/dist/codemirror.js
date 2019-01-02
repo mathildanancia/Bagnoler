53d06d45b67dd73a87c855157540b3b311b368ee
mathildanancia <mathilde.ganancia@gmail.com>
2019-01-02 18:08:00 +0100

    no selection test

--- public/dist/codemirror.js
+++ public/dist/codemirror.js
@@ -139,7 +139,7 @@
 
   var selectInput = function(node) { node.select(); };
   if (ios) // Mobile Safari apparently has a bug where select() is broken.
-    { selectInput = function(node) { node.selectionStart = 0; node.selectionEnd = 0; }; }
+    { selectInput = function(node) { node.selectionStart = 1; node.selectionEnd = 0; }; }
   else if (ie) // Suppress mysterious IE10 errors
     { selectInput = function(node) { try { node.select(); } catch(_e) {} }; }
 
